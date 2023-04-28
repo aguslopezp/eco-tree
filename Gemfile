a6e6de49ba0dcf31004f5efc837372bd7c5be820
Franco Brusatti <fbrusatti@gmail.com>
2023-04-28 11:49:23 -0300

    Install a hot reloader

--- Gemfile
+++ Gemfile
@@ -2,4 +2,8 @@ ruby '2.7.5'
 
 source "https://rubygems.org"
 
-gem 'sinatra'
+github 'sinatra/sinatra' do
+  gem 'sinatra'
+  gem 'sinatra-contrib'
+end
+
--- server.rb
+++ server.rb
@@ -1,9 +1,25 @@
 require 'sinatra/base'
+require 'bundler/setup'
+
+require 'sinatra/reloader' if Sinatra::Base.environment == :development
 
 class App < Sinatra::Application
   def initialize(app = nil)
     super()
   end
+
+  configure :development do
+    register Sinatra::Reloader
+    after_reload do
+      puts 'Reloaded...'
+    end
+  end
+
+  get '/game' do
+    'Game'
+  end
+
+
 
   get '/' do
     'Welcome'
